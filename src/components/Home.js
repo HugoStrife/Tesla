778332d96432c3d1457859152a55a6f247f233e9
Hugo Strife <95651091+HugoStrife@users.noreply.github.com>
2022-09-19 17:50:18 +0200

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,9 +11,34 @@ function Home() {
         leftBtnText="Custom Order"
         rightBtnText="Existing Inventory"
       />
-      <Section />
-      <Section />
-      <Section />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-y.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-3.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model X"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-x.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Lowest Cost Solar Panels in Spain"
+        description="Money- back guarantee"
+        backgroundImg="solar-panel.jpg"
+        leftBtnText="Order now"
+        rightBtnText="Learn more"
+      ></Section>
     </Container>
   );
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,16 +1,22 @@
 import React from "react";
 import styled from "styled-components";
-function Section(props) {
+function Section({
+  title,
+  description,
+  leftBtnText,
+  rightBtnText,
+  backgroundImg,
+}) {
   return (
-    <Wrap>
+    <Wrap bgImage={backgroundImg}>
       <ItemText>
-        <h1>{props.title}</h1>
-        <p>{props.description}</p>
+        <h1>{title}</h1>
+        <p>{description}</p>
       </ItemText>
       <Buttons>
         <ButtonGroup>
-          <LeftButton>Custom Order</LeftButton>
-          <RightButton>Existing Inventory</RightButton>
+          <LeftButton>{leftBtnText}</LeftButton>
+          <RightButton>{rightBtnText}</RightButton>
         </ButtonGroup>
         <DownArrow src="/images/down-arrow.svg" />
       </Buttons>
@@ -25,7 +31,7 @@ const Wrap = styled.div`
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
-  background-image: url("/images/model-s.jpg");
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
   display: flex;
   flex-direction: column;
   justify-content: space-between; //VERTICAL ALIGNMENT
@@ -70,6 +76,7 @@ const DownArrow = styled.img`
   height: 40px;
   overflow-x: hidden;
   animation: animateDown infinite 1.5s;
+  cursor: pointer;
 `;
 
 const Buttons = styled.div``;
